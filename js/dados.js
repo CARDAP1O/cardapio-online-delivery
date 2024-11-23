3ae2432b0429716eb3f3abaa0c59228cdc416081
lucas <lucasaoff84@gmail.com>
2024-11-23 19:00:47 -0300

    cardapioOnline

--- js/dados.js
+++ js/dados.js
@@ -1,5 +1,5 @@
 var MENU = {
-    "churrasco": [
+  /*  "churrasco": [
         {
             "id": "ribs-brisket-and-burnt-ends",
             "img": "./img/cardapio/churrasco/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg",
@@ -84,179 +84,101 @@ var MENU = {
             "dsc": "Best of Texas BBQ Combo - Serves 14",
             "price": 269
         }
-    ],
+    ],*/
     "burgers": [
         {
-            "id": "the-gramercy-tavern-burger-4-pack",
-            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
-            "name": "Hambuger tomate",
-            "dsc": "The Gramercy Tavern Burger - 4 Pack",
-            "price": 15
-        },
-        {
-            "id": "shake-shack-shackburger-8-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
-            "name": "Shake Shack",
-            "dsc": "Shake Shack ShackBurger® - 8 Pack",
-            "price": 49
-        },
-        {
-            "id": "gotts-cheeseburger-kit-for-4",
-            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
-            "name": "Gott's Roadside",
-            "dsc": "Gott's Complete Cheeseburger Kit for 4",
-            "price": 99
-        },
-        {
-            "id": "le-big-matt-kit-for-6",
-            "img": "./img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
-            "name": "Emmy Squared",
-            "dsc": "Le Big Matt Burger Kit for 6",
-            "price": 99
-        },
-        {
-            "id": "shake-shack-shackburger-16-pack",
-            "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
-            "name": "Shake Shack",
-            "dsc": "Shake Shack Shackburger® - 16 Pack",
-            "price": 89
-        },
-        {
-            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
-            "img": "./img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
-            "name": "Peter Luger Steak House",
-            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
-            "price": 175.95
-        },
-        {
-            "id": "double-stack-burger-kit-for-4",
-            "img": "./img/cardapio/burguers/handf-double-stack-burger-kit-for-4.4ee9f54b1d6087e9996335f07c13e5cd.jpg",
-            "name": "Holeman & Finch",
-            "dsc": "Double Stack Burger Kit for 4",
-            "price": 79
-        },
-        {
-            "id": "goldbelly-burger-bash-pack",
-            "img": "./img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
-            "name": "Pat LaFrieda Meats",
-            "dsc": "Goldbelly \"Burger Bash\" Pack",
-            "price": 109
-        },
-        {
-            "id": "burger-au-poivre-kit-4-pack",
-            "img": "./img/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg",
-            "name": "Raoul's",
-            "dsc": "Burger Au Poivre Kit - 4 Pack",
-            "price": 99
-        },
-        {
-            "id": "goldbelly-burger-blend-4-lbs",
-            "img": "./img/cardapio/burguers/goldbelly-burger-blend-1-lb.13a21b66edf7173a59c75c3a6d2f981b.jpg",
-            "name": "Flannery Beef",
-            "dsc": "Goldbelly Burger Blend - 4 lbs.",
-            "price": 79
-        },
-        {
-            "id": "gotts-complete-cheeseburger-kit-for-8",
-            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-8.092aa049d00286fa1733d720decc782e.jpg",
-            "name": "Gott's Roadside",
-            "dsc": "Gott's Complete Cheeseburger Kit for 8",
-            "price": 149
-        },
-        {
-            "id": "gramercy-tavern-burger-kielbasa-combo",
-            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
-            "name": "Gramercy Tavern",
-            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
-            "price": 149
-        }
-    ],
-    "sobremesas": [
-        {
             "id": "15259-german-chocolate-killer-brownie-tin-pack",
-            "img": "./img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
-            "name": "Killer Brownie®",
-            "dsc": "German Chocolate Killer Brownie®",
-            "price": 39.99
+            "img": "./img/cardapio/AÇAI/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
+            "name": "NUTELA®",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDENSADO E NUTELA®",
+            "price": 20.00
         },
         {
             "id": "jacques-world-famous-chocolate-chip-cookies",
-            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
-            "name": "Jacques Torres Chocolate",
-            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
-            "price": 39.95
+            "img": "./img/cardapio/AÇAI/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
+            "name": "MORANGO",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E MORANGO",
+            "price": 20.00
         },
         {
             "id": "luigis-original-cannoli-pie",
-            "img": "./img/cardapio/sobremesas/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg",
-            "name": "The Cannoli Pie Company",
-            "dsc": "Original Cannoli Pie",
-            "price": 69
+            "img": "./img/cardapio/AÇAI/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
+            "name": "BANANA",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E BANANA",
+            "price": 20.00
         },
         {
             "id": "sea-salted-caramel-swirl-cheesecake",
-            "img": "./img/cardapio/sobremesas/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
-            "name": "Cotton Blues Cheesecake Company",
-            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
-            "price": 65
+            "img": "./img/cardapio/AÇAI/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
+            "name": "ABACAXI",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E ABACAXI",
+            "price": 20.00
         },
         {
             "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
-            "img": "./img/cardapio/sobremesas/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
-            "name": "Brooklyn Blackout Company",
-            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
-            "price": 89
+            "img": "./img/cardapio/AÇAI/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a78.jpg",
+            "name": "MAÇA",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E MAÇA",
+            "price": 20.00
         },
         {
             "id": "best-seller-cupcake-dozen",
-            "img": "./img/cardapio/sobremesas/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
-            "name": "Crave Cupcakes",
-            "dsc": "Best Seller Cupcake Dozen",
-            "price": 89
+            "img": "./img/cardapio/AÇAI/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08ff.jpg",
+            "name": "MIX COMPLETO",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA, MAÇA, MORANGO E BANANA",
+            "price": 23.00
         },
-        {
-            "id": "choose-your-own-ice-cream-donuts-6-pack",
-            "img": "./img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
-            "name": "Elegant Desserts",
-            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
-            "price": 69
-        },
-        {
-            "id": "17481-jewish-dessert-3-pack",
-            "img": "./img/cardapio/sobremesas/jewish-classics-dessert-pack.7d4b76630f2fe75dcb7bbcf2501b7390.jpg",
-            "name": "Ess-a-Bagel",
-            "dsc": "Jewish Classics Dessert Pack",
-            "price": 89.95
-        },
-        {
-            "id": "dessert-bar-care-package",
-            "img": "./img/cardapio/sobremesas/dessert-bar-care-package.324aa28fe98c9dc67b75aac43376902e.jpg",
-            "name": "Bread and Roses Bakery",
-            "dsc": "Dessert Bar Care Package",
-            "price": 65
-        },
-        {
-            "id": "donut-cookies-12-pack",
-            "img": "./img/cardapio/sobremesas/donut-cookies-12-pack.46f42c61c4a79fd2051a99b2f23e890e.jpg",
-            "name": "Stan's Donuts",
-            "dsc": "Donut Cookies - 12 Pack",
-            "price": 49
-        },
-        {
-            "id": "gulab-jamun-ice-cream-cakes-2-pack",
-            "img": "./img/cardapio/sobremesas/gulab-jamun-ice-cream-cakes-2-pack.c45b4d0750ad22d741f84dc1f26d20e7.jpg",
-            "name": "Malai Ice Cream",
-            "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
-            "price": 79
-        },
-        {
-            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
-            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-12-pack.3b373bdd67cd25084182c21499f675d1.jpg",
-            "name": "Jacques Torres Chocolate",
-            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
-            "price": 69.95
-        }
+   
     ],
+    "sobremesas": [
+
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/AÇAI/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
+            "name": "NUTELA®",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDENSADO E NUTELA®",
+            "price": 23.00
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/AÇAI/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
+            "name": "MORANGO",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E MORANGO",
+            "price": 23.00
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/AÇAI/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
+            "name": "BANANA",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E BANANA",
+            "price": 22.00
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/cardapio/AÇAI/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
+            "name": "ABACAXI",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E ABACAXI",
+            "price": 22.00
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/cardapio/AÇAI/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a78.jpg",
+            "name": "MAÇA",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA E MAÇA",
+            "price": 22.00
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/cardapio/AÇAI/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08ff.jpg",
+            "name": "MIX COMPLETO",
+            "dsc": "LEITE EM PÓ, AÇAI, LEITE CONDESADO, GRANOLA, MAÇA, MORANGO E BANANA",
+            "price": 25.00
+        },
+   
+   
+       
+    ],
+    /*
     "bebidas": [
         {
             "id": "hong-kong-boba-tea-kit-for-6",
@@ -514,5 +436,5 @@ var MENU = {
             "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
             "price": 165
         }
-    ]
+    ]*/
 }
